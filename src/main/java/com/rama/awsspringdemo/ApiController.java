d97e05f1734450120373cb4e13d1281992791692
ramakrishna-ncompass <ramakrishna@ncompass.in>
2021-10-12 11:27:57 +0530

    initial commit

---
+++ src/main/java/com/rama/awsspringdemo/ApiController.java
@@ -0,0 +1,13 @@
+package com.rama.awsspringdemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("api/health")
+    public ResponseEntity<?> healthCheck(){
+        return ResponseEntity.ok("Works successfully");
+    }
+}
---
+++ src/test/java/com/rama/awsspringdemo/AwsSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rama.awsspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
